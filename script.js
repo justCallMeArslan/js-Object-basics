a87c5c92723d28f2de545673cdf0164b2280e87d
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-19 12:02:12 +0300

    Complete JS.info up to Property value shorthand.

--- script.js
+++ script.js
@@ -35,3 +35,59 @@ country: "China", // last property in the list may end with comma for easier add
 
 
 // Square brackets
+
+
+// user3.likes birds = true; - will return syntax error, as birds in that case is unexpected value.
+// There is an alernative "square bracket notation" that works with any string:
+
+let user4 = {};
+
+user4["likes birds"] = true;
+console.log(user4);
+delete user4["likes birds"];
+console.log(user4);
+
+let key = "likes birds";
+user4[key] = true; // - same as user4["likes birds"] = true;
+console.log(user4);
+
+let favQuote = "What is your favorite quote?"; // should be a promt, but noisy
+let quote = "quote";
+
+user4.quote = favQuote; // doesnt return anything, undefined
+user4[quote] = favQuote; // creates key and adds value to that key , returns {quote: favQuote}
+console.log(user4);
+
+// computed properties - can be used in an object as a key :
+
+let fruit = "apple";
+// prompt ("You the most loved fruuit", "Choose any"); // apple
+
+let array = ["apple", "cucmuber", "banana"];
+let bag = {
+    [fruit]: 5, //the name of the key propery taken from the variable fruit
+    [array]: 10,
+};
+
+bag[array]; // array also can become a key property for the object
+
+console.log(bag); // { apple:5, apple,cucumber, banana: 10};
+
+// can be used in more complex way:
+
+let cars =  "BMW";
+let dealerShop = {
+    [cars + " is super modern car. Guarantee for "]:100 + " %" // 
+};
+
+console.log(dealerShop);
+
+
+
+
+
+
+
+
+
+
