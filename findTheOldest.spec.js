93883e54e76a96fd7f9c4fd91e69837023db20a2
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-24 17:35:49 +0300

    Learn how to run JEST tests. Set up.

--- findTheOldest.spec.js
+++ findTheOldest.spec.js
@@ -1,7 +1,23 @@
-const helloWorld = require('./findTheOldest');
+const findeTheOldest = require('./findTheOldest');
+const findTheYoungest = require('./findTheOldest');
 
-describe('helloWorld', () => {
-    test('prints Hello World', () => {
-    expect(helloWorld("Hello World")).toBe("Hello World");
-    })
+describe('findTheOldest', () => {
+    test('checking who is the oldest of inventors', () => {
+        const inventors = [
+            { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+            { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+            { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+        ];
+        expect(findeTheOldest(inventors).first).toBe("Albert");
+    });
+    test('if missing year, check for youngest', () => {
+        const inventors = [
+            { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+            { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+            { first: 'Galileo', last: 'Galilei', passed: 1642 },
+        ];
+        expect(findTheYoungest(inventors).first).toBe("Galileo");
+    });
+
+
 });
