3712b3eea6fc6eb7a1198acb2647909fc6417300
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-24 17:34:27 +0300

    Create practice file for reduce.

--- findTheOldest.js
+++ findTheOldest.js
@@ -1,18 +1,40 @@
 
+const randomDateOfBirth = Math.floor(Math.random() * (200 - 150) + 1600);
 
-const helloWorld = function (str){
-    return str;
+const findTheOldest = function (array) {
+    return array.reduce((first, next) => {
+        const lifeTimeFirst = first.passed - first.year;
+        const lifeTimeNext = next.passed - next.year;
+
+        return lifeTimeNext > lifeTimeFirst ? next : first;
+        
+    });
 }
 
 
-console.log(helloWorld());
+const findTheYoungest = function(array){
+    return array.reduce((youngest, current) => {
+
+        const youngestYear = youngest.year ?? randomDateOfBirth;
+        const currentYear = current.year ?? randomDateOfBirth;
+
+        const youngestInvAge = youngest.passed - youngestYear;
+        const currentInvAge = current.passed - currentYear;
+
+        return currentInvAge < youngestInvAge ? current : youngest;
+        
+    })
+}
+
+
+// console.log(findTheOldest(inventors));
+// console.log(findTheYoungest(inventors));
+
+
+//  console.log(Math.floor(Math.random() * (200 - 180)) + 1600);
 
 
 
 
-
-
-
-
-
-module.exports = helloWorld;
+module.exports = findTheOldest;
+module.exports = findTheYoungest;
